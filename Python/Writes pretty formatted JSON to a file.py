5cc7458b416223e84cc0934f20f4dfe7d624367b
Gianluca O <gianopp@gmail.com>
2023-03-11 21:01:39 -0500

    Renamed scripts

---
+++ Python/Writes pretty formatted JSON to a file.py
@@ -0,0 +1,4 @@
+import json
+
+#                                           {} is the JSON data being printed to file
+with open('TEMP.json', 'w') as f: json.dump({}, f, indent="\t")
--- Writes pretty formatted JSON to a file.py
+++
@@ -1,4 +0,0 @@
-import json
-
-#r is the json data being sent to file
-with open('TEMP.json', 'w') as f: json.dump(r.json(), f, indent="\t")
