2674e17d61ce0d0203bb653013059f3466d8db09
Gianluca Oppedisano <gianluca.oppedisano@prodrive-technologies.com>
2024-01-05 11:06:55 -0500

    Update countdown timer.py

--- countdown timer.py
+++ countdown timer.py
@@ -17,7 +17,16 @@ def countdown(timer: int, timer_message_prefix: str = '', timer_message_suffix: 
             message = f'{message} ' # Add spaces to end of line so previous entire line is replaced if new message is shorter
         print(message, end='\r')
         timer -= 1
+        time.sleep(1)
     remove_message = ' ' * max_str_length
     print(remove_message, end='\r')
     return max_str_length
 
+countdown(3)
+print('Done!')
+
+countdown(3, 'In ')
+print('Done!')
+
+countdown(3, 'In ', ' seconds')
+print('Done!')
