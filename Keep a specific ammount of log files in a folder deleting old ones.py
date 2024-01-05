57bcef07c106955889c187d3b8aecd080277a177
Gianluca Oppedisano <gianluca.oppedisano@prodrive-technologies.com>
2024-01-05 10:48:16 -0500

    Update Keep a specific ammount of log files in a folder deleting old ones.py

--- Keep a specific ammount of log files in a folder deleting old ones.py
+++ Keep a specific ammount of log files in a folder deleting old ones.py
@@ -1,40 +1,41 @@
 import logging
-from pathlib import Path
 import os
+from pathlib import Path
+logger = logging.getLogger(__name__)
 
 def remove_old_log_files(logs_to_keep: int):
-	"""
-	Removes old log files in the 'logs' directory, keeping a specified number of logs.
-	
-	Args:
-		logs_to_keep (int): The number of most recent log files to keep.
-	"""
-
-	logging.info('Deleting old log files.')
-	try:
-		logs_dir = Path('logs')
-		if not logs_dir.exists():
-			logging.exception(f'Logs directory "{logs_dir}" not found.')
-			raise
-		
-		logging.debug('Getting list of log files to delete.')
-		old_logs = list(sorted(logs_dir.iterdir(), key=os.path.getctime, reverse=False))[1:]
-		old_logs.reverse()
-		old_logs = old_logs[logs_to_keep:]
-		logging.info(f'Found {len(old_logs)} log(s) to remove.')
-		
-		if len(old_logs) > 1:
-			old_logs_deleted = 0
-			for old_log in old_logs:
-				try:
-					os.remove(old_log)
-					logging.info(f'Deleted "{old_log}".')
-					old_logs_deleted += 1
-				except FileNotFoundError:
-					logging.warning(f'Cannot delete old log file "{old_log}". File not found.')
-				except Exception as e:
-					logging.warning(f'Cannot delete old log file "{old_log}". {repr(e)}')
-			logging.info(f'Deleted {old_logs_deleted} old log(s).')
-	
-	except Exception as e:
-		logging.warning(f'An error occurred while removing old log files: {repr(e)}')
+    """
+    Removes old log files in the 'logs' directory, keeping a specified number of logs.
+    
+    Args:
+        logs_to_keep (int): The number of most recent log files to keep.
+    """
+    
+    logger.info('Deleting old log files...')
+    try:
+        logs_dir = Path('logs')
+        if not logs_dir.exists():
+            logger.exception(f'Logs directory "{logs_dir}" not found.')
+            raise
+        
+        logger.debug('Getting list of log files to delete...')
+        old_logs = list(sorted(logs_dir.iterdir(), key=os.path.getctime, reverse=False))[1:]
+        old_logs.reverse()
+        old_logs = old_logs[logs_to_keep:]
+        logger.info(f'Found {len(old_logs)} log(s) to delete.')
+        
+        if len(old_logs) > 1:
+            old_logs_deleted = 0
+            for old_log in old_logs:
+                try:
+                    os.remove(old_log)
+                    logger.info(f'Deleted "{old_log}".')
+                    old_logs_deleted += 1
+                except FileNotFoundError:
+                    logger.warning(f'Cannot delete old log file "{old_log}". File not found.')
+                except Exception as e:
+                    logger.warning(f'Cannot delete old log file "{old_log}". {repr(e)}')
+            logger.info(f'Deleted {old_logs_deleted} old log(s).')
+    
+    except Exception as e:
+        logger.warning(f'An error occurred while removing old log files: {repr(e)}')
