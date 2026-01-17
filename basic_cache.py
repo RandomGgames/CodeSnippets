6ec8398cbdc677386d9bced6f278dec3e847d3b7
RandomGgames <gianopp@gmail.com>
2026-01-17 18:19:27 -0500

    Update basic_cache.py

--- basic_cache.py
+++ basic_cache.py
@@ -1,11 +1,16 @@
+"""
+Cache module for saving and loading data to and from a file.
+"""
+
 import json
 import logging
-import pathlib
+from pathlib import Path
 import typing
+
 logger = logging.getLogger(__name__)
 
 
-def load_cache(path: typing.Union[pathlib.Path, str] = "cache.json") -> dict:
+def load_cache(path: typing.Union[Path, str] = "cache.json") -> dict:
     """
     Loads a cache from the given path.
 
@@ -16,7 +21,7 @@ def load_cache(path: typing.Union[pathlib.Path, str] = "cache.json") -> dict:
     dict: The loaded cache.
     """
     logger.debug("Loading cache...")
-    path = pathlib.Path(path)
+    path = Path(path)
     if path.exists():
         try:
             logger.debug("Reading cache file...")
@@ -43,13 +48,13 @@ def validate_cache(data: dict) -> None:
     """
     logger.debug("Validating cache...")
     for file_path in list(data["files"].keys()):
-        if not pathlib.Path(file_path).exists():
+        if not Path(file_path).exists():
             logger.debug("Removing non-existent file %s from cache.", json.dumps(str(file_path)))
             del data["files"][file_path]
     logger.debug("Cache validated successfully.")
 
 
-def save_cache(data: dict, path: typing.Union[pathlib.Path, str] = "cache.json") -> None:
+def save_cache(data: dict, path: typing.Union[Path, str] = "cache.json") -> None:
     """
     Saves the given cache data to the given path.
 
@@ -58,24 +63,23 @@ def save_cache(data: dict, path: typing.Union[pathlib.Path, str] = "cache.json")
     path (typing.Union[pathlib.Path, str], optional): The path of the cache file to save. Defaults to "cache.json".
     """
     logger.debug("Saving cache...")
-    path = pathlib.Path(path)
+    path = Path(path)
     try:
         cache_dir = path.parent
         if not cache_dir.exists():
             cache_dir.mkdir(parents=True)
             logger.debug("Created cache directory %s.", json.dumps(str(cache_dir)))
-            with open(path, "w", encoding="utf-8") as f:
-                json.dump(data, f, indent=4)
-            logger.debug("Saved cache.")
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4)
+        logger.debug("Saved cache.")
     except Exception as e:
         logger.error("Failed to save cache to %s due to %s.", json.dumps(str(path)), e)
         raise
 
 
+# Example usage
 cache = load_cache()
-test = {
-    "a": "a",
-    "b": 2,
-    "c": ["c"],
-}
-save_cache(test)
+cache["a"] = "a"
+cache["b"] = 2
+cache["c"] = ["c"]
+save_cache(cache)
