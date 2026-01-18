b84157a59d7969ca9f7a0bc1e88f33ed81799816
RandomGgames <gianopp@gmail.com>
2026-01-18 16:39:15 -0500

    Delete Return blank string if false or None or returns s.py

--- Return blank string if false or None or returns s.py
+++
@@ -1,7 +0,0 @@
-"""
-An example of how to return a blank string if it is either None or False
-"""
-
-# Returns s as a string or returns a blank string s is either None or False
-s = ''
-str(s or '')
