36734aff377e1016812d5ff5cd7ec5c5e06d6a3e
RandomGgames <gianopp@gmail.com>
2026-01-18 16:39:16 -0500

    Create return_blank_string_if_false_or_none.py

---
+++ return_blank_string_if_false_or_none.py
@@ -0,0 +1,7 @@
+"""
+An example of how to return a blank string if it is either None or False
+"""
+
+# Returns s as a string or returns a blank string s is either None or False
+s = ''
+str(s or '')
