c16172fe6b2ed3c4c1d828c96ac15c6573c32c42
RandomGgames <gianopp@gmail.com>
2026-01-17 20:48:48 -0500

    Update Time section of code takes.py

--- Time section of code takes.py
+++ Time section of code takes.py
@@ -1,13 +1,20 @@
+"""
+An example of how to measure the time it takes to run a section of code
+"""
+
 import time
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 # Run this at the start of the function
-start_time = time.perf_counter()
+start_ns = time.perf_counter_ns()
 
-print(f'Start')
+print("Start")
 time.sleep(1)
-print(f'End')
+print("End")
 
-# Run this at the end of function
-end_time = time.perf_counter()
-duration = end_time - start_time
-print(f'Took {duration:.10f}s to complete.')
+end_ns = time.perf_counter_ns()
+duration_s = (end_ns - start_ns) / 1e9
+print(f"Execution completed in {duration_s}s.")
